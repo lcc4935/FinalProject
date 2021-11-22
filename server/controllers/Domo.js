e70a635d7939dc77af518f79ae3182a9764fc35f
Lauren <lcc4935@rit.edu>
2021-11-22 03:52:37 -0500

    D

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -46,7 +46,7 @@ const getDomos = (request, response) => {
   return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
     if (err) {
       console.log(err);
-      return res.status(400),json({ error: 'An error occurred'});
+      return res.status(400).json({ error: 'An error occurred'});
     }
 
     return res.json({ domos: docs });
